d17e8a34e1c70888274b823a772efebeb03c6877
Sofyan Daud <lody.fish@gmail.com>
2016-06-08 13:45:15 +0700

    Update H_Cocohuts.php

--- H_Cocohuts.php
+++ H_Cocohuts.php
@@ -6,15 +6,26 @@
 <?php require 'navigator.php'
        ?>
 <!-- Panels --> 
+
+<?php 
+$mysqli = new mysqli('localhost','root','','bookingyuk');
+
+$sql = "select * from manajement_hotel where judul='Hotel Cocohuts'";
+$result = mysqli_query($mysqli,$sql);
+while ( $row = mysqli_fetch_array($result)) {
+?>
+
 <div class="container">
     <div class="jumbotron">
-        <h1 style="text-align:center">HOTEL COCOHUTS</h1><hr>
+        <h1 style="text-align:center"><?php echo $row['judul'];?></h1><hr>
         <img src="images/Cocohuts/f4.Cocohuts.jpg" class="img-thumbnail" alt="Cocohuts Inn" style="float:left; margin: 50px auto; width: 100%; height: auto;" >
         <div class="menu-contact">
-	            <p>CocoHuts Inn Karimun Jawa salah satu tempat terbaik untuk tinggal, makan dan menikmati pemandangan yang menakjubkan di Pulau Karimun Jawa. penginapan ini mempunyai fasilitas Internet yang cukup cepat. Pemandangan dari restoran menakjubkan. Tidak semua fasilitas bekerja, tapi pemilik besar untuk membantu Anda menemukan solusi. Tempat tidur baru dan benar-benar nyaman. Di restoran mereka membuat pizza terbaik yang pernah saya makan di Asia. Kami memiliki waktu yang besar dan itu benar-benar layak harga.</p>
+	            <p><?php echo $row['keterangan'];?></p>
         </div>
-    </div>
-
+</div>
+ <?php
+}
+?>
 	<div class="panel panel-default panels-color menu-conten">
 	  	<div class="panel-body">
 			<h1>Jenis Kamar</h1>
@@ -29,17 +40,8 @@
 		        </div>
 		        <div class="clearfix visible-sm-block"></div>
 		        <div class="col-sm-6 col-md-4 col-lg-6">
-		            <h2>Bungalow with Sea View</h2><hr>
-		           		 <img src="images/Cocohuts/f5.Cocohuts.jpg" class="img-thumbnail" alt="Bungalow with Sea View" width="250" height="350" style="float:left;">
-		           		 <div class="space-hotel col-sm-6 col-md-4 col-lg-6"> 
-							<p>Bungalow ini memiliki dua pintu masuk pribadi , area tempat duduk dan pemandangan laut yang indah di atas bukit karimunjawa. Ini lengkapi UMC TV AC WIFI dan fasilitas lainnya yang terdapat didalamnya.</p>
-		            		<p><a class="button-booking">Rp.500.000</a> <a href="formulir.php" target="_blank" class="btn btn-success btn-lg">Booking Now</a></p>
-		            	</div>
-		        </div>
-		        <div class="clearfix visible-sm-block"></div>
-		        <div class="col-sm-6 col-md-4 col-lg-6">
-		            <h2>Deluxe Bungalow with Sea View</h2><hr>
-		           		 <img src="images/Cocohuts/f6.Cocohuts.jpg" class="img-thumbnail" alt="Deluxe Bungalow with Sea View" width="250" height="350" style="float:left;">
+		            <h2>Deluxe Bungalow</h2><hr>
+		           		 <img src="images/Cocohuts/f6.Cocohuts.jpg" class="img-thumbnail" alt="Deluxe Bungalow" width="250" height="350" style="float:left;">
 						<div class="space-hotel col-sm-6 col-md-4 col-lg-6"> 
 							<p>Bungalow ini memiliki balkon, pandangan dan swasta masuk. lantai pertama dilengkapi dengan kamar mandi dan ruang duduk, sementara kamar tidur di lantai dua. Kedua lantai memiliki balkon dengan pemandangan yang luar biasa. Balkon lantai dua yang sempurna untuk melihat bintang.</p>
 							<p><a class="button-booking">Rp.600.000</a> <a href="formulir.php" target="_blank" class="btn btn-success btn-lg">Booking Now</a></p>
@@ -54,6 +56,15 @@
 							   <p><a class="button-booking">Rp.300.000</a> <a href="formulir.php" target="_blank" class="btn btn-success btn-lg">Booking Now</a></p>
 		            	</div>
 		        </div>
+		        <div class="clearfix visible-sm-block"></div>
+		        <div class="col-sm-6 col-md-4 col-lg-6">
+		            <h2>Bungalow</h2><hr>
+		           		 <img src="images/Cocohuts/f5.Cocohuts.jpg" class="img-thumbnail" alt="Bungalow" width="250" height="350" style="float:left;">
+		           		 <div class="space-hotel col-sm-6 col-md-4 col-lg-6"> 
+							<p>Bungalow ini memiliki dua pintu masuk pribadi , area tempat duduk dan pemandangan laut yang indah di atas bukit karimunjawa. Ini lengkapi UMC TV AC WIFI dan fasilitas lainnya yang terdapat didalamnya.</p>
+		            		<p><a class="button-booking">Rp.500.000</a> <a href="formulir.php" target="_blank" class="btn btn-success btn-lg">Booking Now</a></p>
+		            	</div>
+		        </div>
 		    </div>
 	  	</div>
 	</div>
@@ -65,7 +76,7 @@
 			<div class="row">
 		        <div class="col-sm-6 col-md-4 col-lg-6">
 		            <h2>Kamar Tidur</h2><hr>
-		            	<img src="images/Cocohuts/f1.Cocohuts.jpg" class="img-thumbnail" alt="Kamar Tidur" width="200" height="270" style="float:left;">
+		            	<img src="images/Cocohuts/f1.Cocohuts.jpg" class="img-thumbnail" alt="Kamar Tidur" width="250" height="400" style="float:left;">
 		        </div>
 		        <div class="clearfix visible-sm-block"></div>
 		        <div class="col-sm-6 col-md-4 col-lg-6">
@@ -93,5 +104,7 @@
     <!-- memanggil file js --> 
 	<script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script src="js/scrollup.js"></script>
 </body>
 </html>
